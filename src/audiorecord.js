158733fa09bfffc39adf634d502a1058c249b485
Ceramos96 <tranquangkha2011@gmail.com>
2018-08-02 09:48:20 +0700

    Enabled file upload(Dropbox), record mixed audio streams

--- src/audiorecord.js
+++ src/audiorecord.js
@@ -2,6 +2,30 @@
 
 //import 'isomorphic-fetch';
 
+var dropboxOptions = {
+  // Success is called once all files have been successfully added to the user's
+  // Dropbox, although they may not have synced to the user's devices yet.
+  success: function () {
+      // Indicate to the user that the files have been saved.
+      alert("Success! Files saved to your Dropbox.");
+  },
+
+  // Progress is called periodically to update the application on the progress
+  // of the user's downloads. The value passed to this callback is a float
+  // between 0 and 1. The progress callback is guaranteed to be called at least
+  // once with the value 1.
+  progress: function (progress) {},
+
+  // Cancel is called if the user presses the Cancel button or closes the Saver.
+  cancel: function () {},
+
+  // Error is called in the event of an unexpected response from the server
+  // hosting the files, such as not being able to find a file. This callback is
+  // also called if there is an error on Dropbox or if the user is over quota.
+  error: function (errorMessage) {}
+};
+
+
 var startButton = document.getElementById('startButton');
 var callButton = document.getElementById('callButton');
 var hangupButton = document.getElementById('hangupButton');
@@ -214,6 +238,20 @@ function stopRecording() {
   mediaRecorder.stop();
   console.log('Recorded Blobs: ', recordedBlobs);
   remoteVideo.controls = true;
+  const blob = new Blob(recordedBlobs, {type: 'audio/ogg'});
+  const url = window.URL.createObjectURL(blob);
+  //var url;
+  blobToDataURL(blob, function(dataurl){
+    var dropboxButton = Dropbox.createSaveButton(dataurl, 'Test.mp3', dropboxOptions);
+  document.getElementById("container").appendChild(dropboxButton);
+  });
+  console.log('Object URL: ',url);
+}
+
+function blobToDataURL(blob, callback) {
+  var a = new FileReader();
+  a.onload = function(e) {callback(e.target.result);}
+  a.readAsDataURL(blob);
 }
 
 
